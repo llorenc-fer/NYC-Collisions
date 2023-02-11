65dae31a5a5fd929db245441e1b7a265072e7cc3
Llorenç Fernandez <119510686+llorenc-fer@users.noreply.github.com>
2023-02-11 17:31:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://github.com/llorenc-fer/NYC-Collisions/blob/main/ICFF-Landing-Page-Header-Background-NYC.png?raw=true");
+             background-image: url("https://github.com/llorenc-fer/NYC-Collisions/blob/main/photo_2023-02-11_12-57-38.jpg?raw=true");
              background-attachment: fixed;
              background-size: cover
          }}
