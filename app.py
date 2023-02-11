95f3c2aad2ef8202093031d5ff6f2b20e5d74872
Llorenç Fernandez <119510686+llorenc-fer@users.noreply.github.com>
2023-02-11 09:19:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,23 @@ DATA_URL = (
 "https://github.com/chairielazizi/streamlit-collision/blob/master/Motor_Vehicle_Collisions_-_Crashes.csv?raw=true"
 )
 
-#Establecemos la imagen de fondo de la app
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
+
+def add_bg_from_url():
     st.markdown(
-    f"""
-     <style>
-        .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local(r"https://github.com/llorenc-fer/NYC-Collisions/blob/main/ICFF-Landing-Page-Header-Background-NYC.jpg?raw=true")
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://github.com/llorenc-fer/NYC-Collisions/blob/main/ICFF-Landing-Page-Header-Background-NYC.jpg?raw=true");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+
+add_bg_from_url()
 
 st.title("Motor Vehicle Collisions in New York City")
 
